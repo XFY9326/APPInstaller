e731027175dd7ee618468ed85ccdc529a0813168
XFY9326 <1069665464@qq.com>
2018-07-31 22:59:14 +0800

    修复 app图标过大问题

--- app/src/main/java/tool/xfy9326/appinstaller/Methods/ApkMethod.java
+++ app/src/main/java/tool/xfy9326/appinstaller/Methods/ApkMethod.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 import java.util.ArrayList;
@@ -50,13 +55,6 @@ public class ApkMethod {
         return null;
     }
 
-    public Drawable getApplicationIcon() {
-        PackageInfo packageInfo = packageManager.getPackageArchiveInfo(applicationPath, PackageManager.GET_ACTIVITIES);
-        packageInfo.applicationInfo.sourceDir = applicationPath;
-        packageInfo.applicationInfo.publicSourceDir = applicationPath;
-        return packageInfo.applicationInfo.loadIcon(packageManager);
-    }
-
     public String[] getApplicationPermission() {
         PackageInfo packageInfo = packageManager.getPackageArchiveInfo(applicationPath, PackageManager.GET_PERMISSIONS);
         if (packageInfo != null) {
@@ -81,5 +79,44 @@ public class ApkMethod {
             }
         }
         return null;
+    }
+
+    private static Drawable zoomImg(Drawable drawable, int newWidth, int newHeight) {
+        Bitmap bm = drawableToBitmap(drawable);
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap newbmp = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        if (!bm.isRecycled()) {
+            bm.recycle();
+        }
+        return new BitmapDrawable(newbmp);
+    }
+
+    private static Bitmap drawableToBitmap(Drawable drawable) {
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
+        Bitmap bitmap = Bitmap.createBitmap(width, height, config);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, width, height);
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    private static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public Drawable getApplicationIcon(Context context) {
+        PackageInfo packageInfo = packageManager.getPackageArchiveInfo(applicationPath, PackageManager.GET_ACTIVITIES);
+        packageInfo.applicationInfo.sourceDir = applicationPath;
+        packageInfo.applicationInfo.publicSourceDir = applicationPath;
+        Drawable icon = packageInfo.applicationInfo.loadIcon(packageManager);
+        return zoomImg(icon, dip2px(context, 25), dip2px(context, 25));
     }
 }
